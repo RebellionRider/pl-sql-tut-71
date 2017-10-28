b42b923bf4aafb078b89c19da6f450426b00321d
Manish <rebellionrideryt@gmail.com>
2017-10-28 14:47:47 +0530

    PL/SQL sys_RefCursor in Oracle Database by Manish Sharma

---
+++ Script 1.sql
@@ -0,0 +1,13 @@
+SET SERVEROUTPUT ON;
+DECLARE
+    cur_var SYS_REFCURSOR;
+    
+    f_name  employees.first_name%TYPE;
+    emp_sal employees.salary%TYPE;
+BEGIN
+    OPEN cur_var FOR SELECT first_name, Salary FROM employees WHERE employee_id = 100;
+    FETCH cur_var INTO f_name, emp_sal;
+    CLOSE cur_var;
+    DBMS_OUTPUT.PUT_LINE(f_name ||' '||emp_sal);
+END;
+/
